ae044cd2046baedd5112591acf724889a5b17111
Mikko <mjsn@users.noreply.github.com>
2018-01-24 15:56:37 +0200

    Korjaus

--- src/test/java/fi/haagahelia/ohjtek/bookstore/BookstoreApplicationTests.java
+++ src/test/java/fi/haagahelia/ohjtek/bookstore/BookstoreApplicationTests.java
@@ -1,4 +1,4 @@
-package fi.haagahelia.ohjtek.h2t1;
+package fi.haagahelia.ohjtek.bookstore;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class H2t1ApplicationTests {
+public class BookstoreApplicationTests {
 
 	@Test
 	public void contextLoads() {
