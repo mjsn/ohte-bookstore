63756c4cfe47ac34920611d0f310bff3df1aee06
Mikko <mjsn@users.noreply.github.com>
2018-02-12 15:37:18 +0200

    REST, user login/logout

--- src/main/java/fi/haagahelia/ohjtek/bookstore/web/BookstoreController.java
+++ src/main/java/fi/haagahelia/ohjtek/bookstore/web/BookstoreController.java
@@ -1,12 +1,15 @@
 package fi.haagahelia.ohjtek.bookstore.web;
 
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import fi.haagahelia.ohjtek.bookstore.domain.Book;
 import fi.haagahelia.ohjtek.bookstore.domain.BookRepository;
@@ -18,7 +21,17 @@ public class BookstoreController {
 	@Autowired
 	private BookRepository repository;
 
-    @RequestMapping(value="/index", method=RequestMethod.GET)
+    @RequestMapping(value="/books", method = RequestMethod.GET)
+    public @ResponseBody List<Book> bookListRest() {	
+        return (List<Book>) repository.findAll();
+    }    
+
+    @RequestMapping(value="/book/{id}", method = RequestMethod.GET)
+    public @ResponseBody Book findBookRest(@PathVariable("id") Long bookId) {	
+    	return repository.findOne(bookId);
+    }       
+	
+    @RequestMapping(value= {"", "/index", "/index.html"}, method=RequestMethod.GET)
     public String frontpage(Model model) {
         return "index";
     }
@@ -33,7 +46,12 @@ public class BookstoreController {
     public String addBook(Model model){
     	model.addAttribute("book", new Book());
         return "addbook";
-    }     
+    }
+
+    @RequestMapping(value="/login")
+    public String login() {	
+        return "login";
+    }	
     
     @RequestMapping(value = "/save", method = RequestMethod.POST)
     public String save(Book book){
